1d3d8a69691dc3b119918950ae197b1865d12abc
Brice <lebowvsky@gmail.com>
2023-02-06 23:55:11 +0100

    feat: :sparkles: init project

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,6 +21,14 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior: function (to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    }
+  },
 });
 
 export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,107 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <div class="wrapper">
+      <article class="article">
+        <section
+          v-for="(header, index) in headers"
+          :key="index"
+          class="section"
+        >
+          <h2 :id="`index`" class="section-title">{{ header }}</h2>
+          <p class="section-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+            numquam beatae est ut quae ex qui perspiciatis officia distinctio
+            tenetur alias sunt ratione, optio, repellendus molestias ipsa nemo
+            perferendis consequatur. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Impedit temporibus et, accusantium aut nihil
+            voluptatibus nostrum asperiores ratione perferendis reprehenderit ab
+            quam, dolorum amet voluptas omnis adipisci atque quasi minima?
+            Doloribus vitae ad recusandae numquam quos dicta provident tempore
+            nemo fugiat, rerum sunt minus eligendi incidunt qui itaque quod ab
+            eum consectetur laudantium odit consequatur nobis. Eaque quae labore
+            delectus! Amet, facere dignissimos nesciunt corporis alias
+            perspiciatis excepturi! Assumenda dolorum debitis doloribus ipsum
+            inventore repudiandae dolore nemo, eligendi tempora unde minima
+            velit expedita amet esse iusto qui. Consequuntur, nemo molestias!
+            Beatae soluta velit exercitationem sed fugiat molestias totam maxime
+            tempore incidunt maiores, ad earum. Recusandae, tempora! Voluptas
+            vel dicta id ipsa, ipsam, inventore deserunt mollitia officia esse
+            voluptatibus eum facere? Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Veritatis nihil enim eius quasi repellat nostrum
+            obcaecati explicabo placeat voluptatum adipisci, quod, consequuntur
+            veniam exercitationem, quidem corporis dolorum asperiores facilis
+            dicta. Sit neque mollitia obcaecati hic reprehenderit dolorem odio,
+            voluptate magni aliquid tenetur dicta. Minima explicabo molestiae in
+            placeat officia modi? Veniam quaerat saepe molestiae eius placeat
+            vero enim magnam accusantium! Quos est deserunt blanditiis sint
+            eveniet culpa repellat, porro cum maiores! Delectus maiores
+            consequatur tempora ipsam veritatis quo in sequi. Dolore fuga ad
+            quam assumenda, tempora quidem recusandae sint totam!
+          </p>
+        </section>
+      </article>
+    </div>
+    <aside class="nav">
+      <ul>
+        <!-- <li v-for="(header, index) in headers" :key="index">
+          <RouterLink :to="{ name: 'home', hash: '#6' }" class="link">{{
+            header
+          }}</RouterLink>
+        </li> -->
+        <li v-for="(header, index) in headers" :key="index">
+          <RouterLink to="#6" class="link">{{ header }}</RouterLink>
+        </li>
+      </ul>
+    </aside>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
+<script lang="ts" setup>
+const headers = [
+  "Titre 1",
+  "Titre 2",
+  "Titre 3",
+  "Titre 4",
+  "Titre 5",
+  "Titre 6",
+];
 </script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+
+  .wrapper {
+    width: 100%;
+    max-width: 800px;
+  }
+
+  .nav {
+    position: sticky;
+    top: 50px;
+    background-color: rgb(253, 238, 220);
+    border-radius: 5px;
+    align-self: flex-start;
+    padding: 20px;
+    min-width: 200px;
+    ul {
+      list-style-type: none;
+      padding: 0;
+
+      li {
+        margin: 10px 0;
+        padding: 5px;
+        a {
+          text-decoration: none;
+        }
+
+        &.active {
+          background-color: rgb(248, 210, 163);
+        }
+      }
+    }
+  }
+}
+</style>
